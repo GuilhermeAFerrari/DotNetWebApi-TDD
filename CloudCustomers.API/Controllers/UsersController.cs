beba0653566cc64ed4501d46d95b179dfa5d93ac
Guilherme Ferrari <guile.ferrari@hotmail.com>
2023-09-15 12:24:34 -0300

    build: initial commit

---
+++ CloudCustomers.API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using CloudCustomers.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCustomers.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> _logger;
+        private readonly IUsersService _usersService;
+
+
+        public UsersController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        [HttpGet(Name = "GetUsers")]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _usersService.GetAllUsers();
+
+            if (users.Any())
+            {
+                return Ok(users);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ CloudCustomers.UnitTests/Fixtures/UsersFixture.cs
@@ -0,0 +1,35 @@
+﻿using CloudCustomers.API.Models;
+
+namespace CloudCustomers.UnitTests.Fixtures
+{
+    public static class UsersFixture
+    {
+        public static List<User> GetTestUsers() => new()
+            {
+                new User()
+                {
+                    Id = 1,
+                    Name = "Guilherme",
+                    Address = new()
+                    {
+                        City = "World",
+                        Street = "Central",
+                        ZipCode = "1020304050"
+                    },
+                    Email = "gui@gui.com.br"
+                },
+                new User()
+                {
+                    Id = 1,
+                    Name = "Ferrari",
+                    Address = new()
+                    {
+                        City = "World2",
+                        Street = "Central2",
+                        ZipCode = "1020304050-2"
+                    },
+                    Email = "gui2@gui.com.br"
+                },
+            };
+    }
+}
